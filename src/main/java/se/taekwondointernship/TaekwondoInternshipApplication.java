17af2415f410fa44dda7fcd2a98c3be0ee5265a4
Julius <juliusbuchner@gmail.com>
2022-11-24 10:08:05 +0100

    Initial

---
+++ src/main/java/se/taekwondointernship/TaekwondoInternshipApplication.java
@@ -0,0 +1,13 @@
+package se.taekwondointernship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaekwondoInternshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaekwondoInternshipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/taekwondointernship/TaekwondoInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package se.taekwondointernship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaekwondoInternshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
