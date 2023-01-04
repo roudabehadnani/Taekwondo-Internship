588d3fae7f424dad2814cc208e3dcc13269490fb
Roudabeh Adnani <106333638+roudabehadnani@users.noreply.github.com>
2023-01-04 16:23:01 +0100

    Update index.js

--- newMember/index.js
+++ newMember/index.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.css";
+import App from './createNewMember/App';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>  
+   
+);
 
